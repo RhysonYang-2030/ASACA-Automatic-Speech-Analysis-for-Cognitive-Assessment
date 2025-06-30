927f7ebef8d66495fcc9ea1db17b87f1409e553d
Xinbo Yang <xyang2@tcd.ie>
2025-06-30 06:06:35 +0100

    Update __main__.py

--- src/asaca/__main__.py
+++ src/asaca/__main__.py
@@ -1,2 +1,7 @@
-from asaca.cli import main
-main()
+
+import sys
+from asaca.cli import main as cli_main
+
+if __name__ == "__main__":
+    # forward everything after "-m asaca" to the real CLI
+    sys.exit(cli_main(sys.argv[1:]))
